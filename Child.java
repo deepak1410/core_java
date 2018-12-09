1eff21c6e4f5970e8fcdc7ef5e4597d2af605d4a
Deepak Kumar <deepak4ev@gmail.com>
2018-12-09 19:31:38 +0530

    Add files via upload

---
+++ Child.java
@@ -0,0 +1,85 @@
+package com.dpk.inheritance;
+
+class Sample {
+	void send() {
+		System.out.println("Sample send");
+	}
+}
+
+class GrandParent {
+	void send() {
+		System.out.println("Sample send");
+	}
+}
+
+class Parent extends GrandParent{
+	
+	void compose() {
+		System.out.println("Parent compose");
+		this.send();
+	}
+	
+	void send() {
+		System.out.println("Parent send");
+	}
+	
+}
+
+
+public class Child extends Parent{
+
+	void send() {
+		System.out.println("Child send");
+	}
+	
+	void chat() {
+		System.out.println("Child Chat");
+	}
+	
+	public static void main(String[] args) {
+
+		/******** Trick-1 ********/
+		/* You can not cast an object to another object which is incompatible i.e. which is not in the inheritance chain. 
+		  Below code resuults in compile time error. */
+		//Sample child = (Sample) new Child();
+		
+		/******** Trick-2 ********/
+		/* You can try to cast parent class object to Child class but at runtime it will throw ClassCastException */
+		//Child child = (Child) new Parent();
+		
+		/******** Trick-3 ********/
+		/* Execution depends on type of object hence below code will print 
+		    Parent compose
+			Child send 
+		*/
+		Child child = new Child();
+		child.compose();
+		
+		/* As Execution depends on type of object hence below code will print 
+	    	Parent compose
+			Parent send 
+		*/
+		Parent parent = new Parent();
+		parent.compose();
+
+		/******** Trick-4 ********/
+		/* Parent class object will not be able to call child class method */
+		Parent parent1 = new Parent();
+		//parent1.chat(); // This will not work 
+		
+		Parent parent2 = new Child();
+		// parent2.chat();  // Even this will not work
+		((Child) parent2).chat(); // This will work
+		
+		/******** Trick-5 ********/
+		/* Irrespective of the type of object in case of non static methid
+		 * The method selection is from the class of the object 
+		 * Both the code below print Child Send */
+		Parent child1 = new Child();
+		Parent child2 = (Parent)new Child();
+		child1.send();
+		child2.send();
+		
+	}
+
+}
